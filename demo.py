bf5486334b76c0019599dbb36090c7c1e3632895
Felix Richards <felixarichards@gmail.com>
2019-03-13 19:53:10 +0000

    changes for batch size dimension

--- demo.py
+++ demo.py
@@ -9,7 +9,7 @@ class IGCN(nn.Module):
     def __init__(self):
         super(IGCN, self).__init__()
         self.features = nn.Sequential(
-            IGConv(3, 32, 9, stride=3, no_g=4),
+            IGConv(1, 32, 9, no_g=4),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
             IGConv(32, 96, 7, stride=2, no_g=4),
@@ -18,18 +18,21 @@ class IGCN(nn.Module):
         )
         self.classifier = nn.Sequential(
             nn.Dropout(),
-            nn.Linear(256 * 6 * 6, 4096),
+            nn.Linear(96, 4096),
             nn.ReLU(inplace=True),
             nn.Linear(4096, 10)
         )
 
     def forward(self, x):
+        # print("INPUT Start of forward", x.size())
         x = self.features(x)
-        print(x.size())
-        x = x.view(x.size(0), 256 * 6 * 6)
+        # print("INPUT After features", x.size())
+        x = x.flatten(1)
+        # print("INPUT Reshaped", x.size())
         x = self.classifier(x)
+        # print("INPUT Classified", x.size())
         return x
-
+        
 
 train_loader = torch.utils.data.DataLoader(
     datasets.MNIST('../data', train=True, download=True,
@@ -46,7 +49,6 @@ test_loader = torch.utils.data.DataLoader(
     batch_size=512, shuffle=True)
 
 example = iter(train_loader).next()
-print(example[0].size())
 model = IGCN().cuda()
-test_out = model(example[0])
+test_out = model(example[0].cuda())
 # optimizer = optim.SGD(model.parameters(), lr=0.01)
