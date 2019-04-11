dbb6c806c039f486f4a730bd74f2014b9d07d977
Felix Richards <felixarichards@gmail.com>
2019-04-11 15:12:16 +0100

    tensor size mismatch in backward

--- igcn/igabor.py
+++ igcn/igabor.py
@@ -5,6 +5,7 @@ from torch.nn.modules.conv import _ConvNd
 from torch import nn
 import numpy as np
 import math
+from .vis import FilterPlot
 
 
 class GaborFunction(Function):
@@ -31,10 +32,10 @@ class GaborFunction(Function):
             grad_output (Tensor): gradient from graph.
         """
         input, weight, result = ctx.saved_tensors
-        grad_weight = gabor_gradient(input, weight).unsqueeze_(1).unsqueeze_(1)
+        grad_weight = gabor_gradient(input, weight).unsqueeze_(2).unsqueeze_(2)
         grad_output = match_shape(grad_output, input, False)
         print(input.size(), grad_weight.size(), grad_output.size())
-        return result*grad_output, (input*grad_weight*grad_output).permute(4, 3, 2, 1, 0)
+        return result*grad_output, (input*grad_weight*grad_output).permute(5, 4, 3, 2, 0, 1)
 
 
 def match_shape(x, y, compress=True):
@@ -59,23 +60,28 @@ class IGConv(_ConvNd):
             input_features, output_features, kernel_size,
             stride, padding, dilation, False, (0, 0), 1, bias
         )
-        self.thetas = nn.Parameter(torch.DoubleTensor(no_g)).cuda()
+        self.gabor_params = nn.Parameter(torch.Tensor(2, no_g)).cuda()
         self.need_bias = (bias is not None)
         self.GaborFunction = GaborFunction.apply
+        self.plot = FilterPlot(no_g)
 
     def forward(self, x):
-        # print("INPUT Forward inside IGC", x.size())
-        enhanced_weight = self.GaborFunction(self.weight, self.thetas)
-        # print("WEIGHT After Gabor", enhanced_weight.size())
-        # print("INPUT After Gabor", x.size())
-        return F.conv2d(x, enhanced_weight, None, self.stride,
+        enhanced_weight = self.GaborFunction(self.weight, self.gabor_params)
+        
+        out = F.conv2d(x, enhanced_weight, None, self.stride,
                         self.padding, self.dilation)
+        # print(self.weight.size())
+        # print(enhanced_weight.size())
+        # self.plot.update(self.weight, self. )
+        return out
+    
+    
 
 
 def gabor(weight, params):
     h = weight.size(2)
     w = weight.size(3)
-    [x, y] = torch.DoubleTensor(np.meshgrid(np.arange(-h/2, h/2), np.arange(-w/2, w/2)))
+    [x, y] = torch.Tensor(np.meshgrid(np.arange(-h/2, h/2), np.arange(-w/2, w/2)))
     if weight.is_cuda:
         x = x.cuda()
         y = y.cuda()
@@ -85,7 +91,7 @@ def gabor(weight, params):
 def gabor_gradient(weight, params):
     h = weight.size(2)
     w = weight.size(3)
-    [x, y] = torch.DoubleTensor(np.meshgrid(np.arange(-h/2, h/2), np.arange(-w/2, w/2)))
+    [x, y] = torch.Tensor(np.meshgrid(np.arange(-h/2, h/2), np.arange(-w/2, w/2)))
     if weight.is_cuda:
         x = x.cuda()
         y = y.cuda()
@@ -107,9 +113,7 @@ def d_s_h(x, y, theta, l):
             torch.sin(2 * math.pi / l * x_prime(x, y, theta))
     b = 2 * math.pi / l ** 2 * x_prime(x, y, theta) *\
             torch.sin(2 * math.pi / l * x_prime(x, y, theta))
-    c = torch.stack([a, b])
-    # print(a.size(), b.size(), c.size())
-    return c
+    return torch.stack([a, b])
 
 
 def x_prime(x, y, theta):
