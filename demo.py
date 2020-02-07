7b9cd15cc0a0af0cd9b64a7dc748fb96f36f7b48
Felix Richards <felixarichards@gmail.com>
2020-02-07 22:40:03 +0000

    LR scheduler and device detection

--- demo.py
+++ demo.py
@@ -13,19 +13,19 @@ if dset == 'cifar':
     train_loader, test_loader, _ = cifar(batch_size=512)
 
 
-example = iter(train_loader).next()
 classes = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
 model = IGCN(no_g=4, model_name="7", dset=dset).to(device)
 for params in model.parameters():
     print(params.size())
 
 total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-print("Total parameter size: " + str(total_params*32/1000000) + "M")
+print("Total parameter size: " + str(total_params/1000000) + "M")
 
 optimizer = optim.Adam(model.parameters())
-train(model, [train_loader, test_loader], epochs=50, opt=optimizer,
-      device=device)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer)
+train(model, [train_loader, test_loader], epochs=1, opt=optimizer,
+      device=device, sch=scheduler)
 
-example = iter(test_loader).next()
-test_out = model(example[0].cuda())
+example = next(iter(test_loader))
+test_out = model(example[0].to(device))
 imshow(example[0], test_out, classes)
