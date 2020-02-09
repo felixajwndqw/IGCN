128b2626bab03a65e24a08fdd4d376ad746dce7d
Felix Richards <felixarichards@gmail.com>
2020-02-09 12:45:25 +0000

    Changed model list

--- igcn/models.py
+++ igcn/models.py
@@ -37,9 +37,9 @@ class IGCN(Model):
         if dset == "mnist":
             if model_name == "default" or model_name == "3":
                 modules = [
-                    IGConv(1, 24, 3, rot_pool=rot_pool, padding=1, no_g=no_g, max_gabor=inter_mg, pool_stride=2),
+                    IGConv(1, 16, 3, rot_pool=rot_pool, padding=1, no_g=no_g, max_gabor=inter_mg, pool_stride=2),
                     nn.ReLU(inplace=True),
-                    IGConv(24, 32, 3, rot_pool=rot_pool, padding=1, no_g=no_g, max_gabor=inter_mg),
+                    IGConv(16, 32, 3, rot_pool=rot_pool, padding=1, no_g=no_g, max_gabor=inter_mg),
                     nn.ReLU(inplace=True),
                     IGConv(32, 48, 3, rot_pool=rot_pool, padding=1, no_g=no_g, max_gabor=inter_mg),
                     nn.ReLU(inplace=True),
@@ -48,9 +48,9 @@ class IGCN(Model):
                 ]
             if model_name == "5":
                 modules = [
-                    IGConv(1, 24, 5, rot_pool=rot_pool, padding=2, no_g=no_g, max_gabor=inter_mg, pool_stride=2),
+                    IGConv(1, 16, 5, rot_pool=rot_pool, padding=2, no_g=no_g, max_gabor=inter_mg, pool_stride=2),
                     nn.ReLU(inplace=True),
-                    IGConv(24, 32, 5, rot_pool=rot_pool, padding=2, no_g=no_g, max_gabor=inter_mg),
+                    IGConv(16, 32, 5, rot_pool=rot_pool, padding=2, no_g=no_g, max_gabor=inter_mg),
                     nn.ReLU(inplace=True),
                     IGConv(32, 48, 5, rot_pool=rot_pool, padding=2, no_g=no_g, max_gabor=inter_mg),
                     nn.ReLU(inplace=True),
@@ -59,9 +59,9 @@ class IGCN(Model):
                 ]
             if model_name == "7":
                 modules = [
-                    IGConv(1, 24, 7, rot_pool=rot_pool, padding=3, no_g=no_g, max_gabor=inter_mg, pool_stride=2),
+                    IGConv(1, 16, 7, rot_pool=rot_pool, padding=3, no_g=no_g, max_gabor=inter_mg, pool_stride=2),
                     nn.ReLU(inplace=True),
-                    IGConv(24, 32, 7, rot_pool=rot_pool, padding=3, no_g=no_g, max_gabor=inter_mg),
+                    IGConv(16, 32, 7, rot_pool=rot_pool, padding=3, no_g=no_g, max_gabor=inter_mg),
                     nn.ReLU(inplace=True),
                     IGConv(32, 48, 7, rot_pool=rot_pool, padding=3, no_g=no_g, max_gabor=inter_mg),
                     nn.ReLU(inplace=True),
@@ -70,9 +70,9 @@ class IGCN(Model):
                 ]
             if model_name == "9":
                 modules = [
-                    IGConv(1, 24, 9, rot_pool=rot_pool, padding=4, no_g=no_g, max_gabor=inter_mg, pool_stride=2),
+                    IGConv(1, 16, 9, rot_pool=rot_pool, padding=4, no_g=no_g, max_gabor=inter_mg, pool_stride=2),
                     nn.ReLU(inplace=True),
-                    IGConv(24, 32, 9, rot_pool=rot_pool, padding=4, no_g=no_g, max_gabor=inter_mg),
+                    IGConv(16, 32, 9, rot_pool=rot_pool, padding=4, no_g=no_g, max_gabor=inter_mg),
                     nn.ReLU(inplace=True),
                     IGConv(32, 48, 9, rot_pool=rot_pool, padding=4, no_g=no_g, max_gabor=inter_mg),
                     nn.ReLU(inplace=True),
@@ -81,22 +81,22 @@ class IGCN(Model):
                 ]
             if model_name == "lp":
                 modules = [
-                    IGConv(1, 24, 9, rot_pool=None, padding=4, no_g=no_g, max_gabor=False),
-                    IGConv(24, 32, 9, rot_pool=False, padding=4, no_g=no_g, max_gabor=False),
+                    IGConv(1, 16, 9, rot_pool=None, padding=4, no_g=no_g, max_gabor=False),
+                    IGConv(16, 32, 9, rot_pool=False, padding=4, no_g=no_g, max_gabor=False, pool_stride=2),
                     nn.ReLU(inplace=True),
                     IGConv(32, 48, 9, rot_pool=None, padding=4, no_g=no_g, max_gabor=False),
-                    IGConv(32, 48, 9, rot_pool=False, padding=4, no_g=no_g, max_gabor=False),
+                    IGConv(48, 48, 9, rot_pool=False, padding=4, no_g=no_g, max_gabor=False),
                     nn.ReLU(inplace=True),
-                    IGConv(48, 64, 9, rot_pool=True, padding=4, no_g=no_g, max_gabor=True),
+                    IGConv(48, 64, 7, rot_pool=False, padding=4, no_g=no_g, max_gabor=True),
                     nn.ReLU(inplace=True)
                 ]
             if model_name == "3c":
                 modules = [
-                    IGConvCmplx(1, 24, 3, rot_pool=rot_pool, padding=1, no_g=no_g, max_gabor=inter_mg),
+                    IGConvCmplx(1, 16, 3, rot_pool=rot_pool, padding=1, no_g=no_g, max_gabor=inter_mg),
                     MaxPoolCmplx(kernel_size=3, stride=2),
                     BatchNormCmplx(),
                     ReLUCmplx(inplace=True),
-                    IGConvCmplx(24, 32, 3, rot_pool=rot_pool, padding=1, no_g=no_g, max_gabor=inter_mg),
+                    IGConvCmplx(16, 32, 3, rot_pool=rot_pool, padding=1, no_g=no_g, max_gabor=inter_mg),
                     MaxPoolCmplx(kernel_size=3, stride=1),
                     BatchNormCmplx(),
                     ReLUCmplx(inplace=True),
@@ -111,11 +111,11 @@ class IGCN(Model):
                 ]
             if model_name == "5c":
                 modules = [
-                    IGConvCmplx(1, 24, 5, rot_pool=rot_pool, padding=2, no_g=no_g, max_gabor=inter_mg),
+                    IGConvCmplx(1, 16, 5, rot_pool=rot_pool, padding=2, no_g=no_g, max_gabor=inter_mg),
                     MaxPoolCmplx(kernel_size=3, stride=2),
                     BatchNormCmplx(),
                     ReLUCmplx(inplace=True),
-                    IGConvCmplx(24, 32, 5, rot_pool=rot_pool, padding=2, no_g=no_g, max_gabor=inter_mg),
+                    IGConvCmplx(16, 32, 5, rot_pool=rot_pool, padding=2, no_g=no_g, max_gabor=inter_mg),
                     MaxPoolCmplx(kernel_size=3, stride=1),
                     BatchNormCmplx(),
                     ReLUCmplx(inplace=True),
@@ -130,11 +130,11 @@ class IGCN(Model):
                 ]
             if model_name == "7c":
                 modules = [
-                    IGConvCmplx(1, 24, 7, rot_pool=rot_pool, padding=3, no_g=no_g, max_gabor=inter_mg),
+                    IGConvCmplx(1, 16, 7, rot_pool=rot_pool, padding=3, no_g=no_g, max_gabor=inter_mg),
                     MaxPoolCmplx(kernel_size=3, stride=2),
                     BatchNormCmplx(),
                     ReLUCmplx(inplace=True),
-                    IGConvCmplx(24, 32, 7, rot_pool=rot_pool, padding=3, no_g=no_g, max_gabor=inter_mg),
+                    IGConvCmplx(16, 32, 7, rot_pool=rot_pool, padding=3, no_g=no_g, max_gabor=inter_mg),
                     MaxPoolCmplx(kernel_size=3, stride=1),
                     BatchNormCmplx(),
                     ReLUCmplx(inplace=True),
@@ -149,11 +149,11 @@ class IGCN(Model):
                 ]
             if model_name == "9c":
                 modules = [
-                    IGConvCmplx(1, 24, 9, rot_pool=rot_pool, padding=4, no_g=no_g, max_gabor=inter_mg),
+                    IGConvCmplx(1, 16, 9, rot_pool=rot_pool, padding=4, no_g=no_g, max_gabor=inter_mg),
                     MaxPoolCmplx(kernel_size=3, stride=2),
                     BatchNormCmplx(),
                     ReLUCmplx(inplace=True),
-                    IGConvCmplx(24, 32, 9, rot_pool=rot_pool, padding=4, no_g=no_g, max_gabor=inter_mg),
+                    IGConvCmplx(16, 32, 9, rot_pool=rot_pool, padding=4, no_g=no_g, max_gabor=inter_mg),
                     MaxPoolCmplx(kernel_size=3, stride=1),
                     BatchNormCmplx(),
                     ReLUCmplx(inplace=True),
@@ -168,17 +168,17 @@ class IGCN(Model):
                 ]
             if model_name == "lpc":
                 modules = [
-                    IGConvCmplx(1, 24, 9, rot_pool=None, padding=4, no_g=no_g, max_gabor=False),
-                    IGConvCmplx(24, 32, 9, rot_pool=False, padding=4, no_g=no_g, max_gabor=False),
-                    MaxPoolCmplx(kernel_size=3, stride=1),
+                    IGConvCmplx(1, 16, 9, rot_pool=None, padding=4, no_g=no_g, max_gabor=False),
+                    IGConvCmplx(16, 32, 9, rot_pool=False, padding=4, no_g=no_g, max_gabor=False),
+                    MaxPoolCmplx(kernel_size=3, stride=2),
                     BatchNormCmplx(),
                     ReLUCmplx(inplace=True),
                     IGConvCmplx(32, 48, 9, rot_pool=None, padding=4, no_g=no_g, max_gabor=False),
-                    IGConvCmplx(32, 48, 9, rot_pool=False, padding=4, no_g=no_g, max_gabor=False),
+                    IGConvCmplx(48, 48, 9, rot_pool=False, padding=4, no_g=no_g, max_gabor=False),
                     MaxPoolCmplx(kernel_size=3, stride=1),
                     BatchNormCmplx(),
                     ReLUCmplx(inplace=True),
-                    IGConvCmplx(48, 64, 9, rot_pool=True, padding=4, no_g=no_g, max_gabor=True),
+                    IGConvCmplx(48, 64, 9, rot_pool=True, padding=3, no_g=no_g, max_gabor=True),
                     MaxPoolCmplx(kernel_size=3, stride=1),
                     BatchNormCmplx(),
                     ReLUCmplx(inplace=True)
