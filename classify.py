181b53a7ec8ff5371fdc62f073f164ba418649a8
Felix Richards <felixarichards@gmail.com>
2020-02-22 13:40:41 +0000

    Results writing

--- classify.py
+++ classify.py
@@ -32,7 +32,7 @@ def write_results(dset, kernel_size, no_g, base_channels,
            "\t" + str(kernel_size) +
            "\t\t" + str(no_g) +
            "\t\t" + str(base_channels) +
-           "\t\t" + str(dropout) +
+           '\t\t' + "{:1.2f}".format(dropout) +
            "\t\t" + str(inter_mg) +
            "\t" + str(final_mg) +
            "\t" + str(cmplx) +
