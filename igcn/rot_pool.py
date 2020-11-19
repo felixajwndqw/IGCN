6430121d420d10cd4a265054d21eb3dd58220f70
Felix Richards <felixarichards@gmail.com>
2020-11-19 14:21:14 +0000

    Fix dependencies

--- igcn/rot_pool.py
+++ igcn/rot_pool.py
@@ -1,4 +1,4 @@
-import torchgeometry as tgm
+# import torchgeometry as tgm
 from torch.nn.modules.pooling import _MaxPoolNd
 import torch.nn.functional as F
 import torch
@@ -14,25 +14,26 @@ class RotMaxPool2d(_MaxPoolNd):
             input (torch.Tensor): Input data to rotate and pool.
             thetas (torch.Tensor): Angles to rotate by. Shape should be [N]
         '''
-        rot = self.rotate(input, thetas)
-        rot_pool = F.max_pool2d(rot, self.kernel_size, self.stride,
-                                self.padding, self.dilation, self.ceil_mode,
-                                self.return_indices)
-        out = self.rotate(rot_pool, -thetas)
-        return out
+        pass
+        # rot = self.rotate(input, thetas)
+        # rot_pool = F.max_pool2d(rot, self.kernel_size, self.stride,
+        #                         self.padding, self.dilation, self.ceil_mode,
+        #                         self.return_indices)
+        # out = self.rotate(rot_pool, -thetas)
+        # return out
 
     def rotate(self, input, thetas):
-
-        rot = torch.empty_like(input)
-        rot_batch = input.size(1) // thetas.size(0)
-        _, _, rows, cols = input.size()
-        centre = rot.new_tensor(((rows-1)/2, (cols-1)/2)) \
-            .unsqueeze(0).repeat(input.size(0), 1)
-        scale = rot.new_ones(input.size(0))
-        for i, th in enumerate(thetas.unsqueeze(1)):
-            M = tgm.get_rotation_matrix2d(centre, th.repeat(input.size(0)), scale)
-            rot[:, i * rot_batch: (i + 1) * rot_batch] = tgm.warp_affine(input[:, i * rot_batch: (i + 1) * rot_batch], M, dsize=(rows, cols))
-        return rot
+        pass
+        # rot = torch.empty_like(input)
+        # rot_batch = input.size(1) // thetas.size(0)
+        # _, _, rows, cols = input.size()
+        # centre = rot.new_tensor(((rows-1)/2, (cols-1)/2)) \
+        #     .unsqueeze(0).repeat(input.size(0), 1)
+        # scale = rot.new_ones(input.size(0))
+        # for i, th in enumerate(thetas.unsqueeze(1)):
+        #     M = tgm.get_rotation_matrix2d(centre, th.repeat(input.size(0)), scale)
+        #     rot[:, i * rot_batch: (i + 1) * rot_batch] = tgm.warp_affine(input[:, i * rot_batch: (i + 1) * rot_batch], M, dsize=(rows, cols))
+        # return rot
 
 
 def main():
